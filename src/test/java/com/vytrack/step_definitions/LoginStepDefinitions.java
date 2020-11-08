1cb002c91950114944c647242b3ac86bae853864
numuminov <numuminov@gmail.com>
2020-11-08 15:01:22 -0500

    fourth two

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,48 +1,66 @@
 package com.vytrack.step_definitions;
-
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utils.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
-    WebDriver driver;
-    @Given("user is on the landing page")
-    public void user_is_on_the_landing_page() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("http://qa1.vytrack.com");
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        Driver.getDriver().get("http://qa1.vytrack.com");
     }
-
-
-
 
     @When("user logs in")
-    public void user_logs_in() throws InterruptedException{
-        driver.findElement(By.id("prependedInput")).sendKeys("salesmanager110");
-        driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123", Keys.ENTER);
+    public void user_logs_in() throws InterruptedException {
+        loginPage.login();
         Thread.sleep(3000);
     }
-    @Then("user should see dashboard  page")
+
+    @Then("user should see dashboard page")
     public void user_should_see_dashboard_page() {
         String expected = "Dashboard";
-        String actual = driver.findElement(By.className("oro-subtitle")).getText().trim();
+        String actual = loginPage.getPageSubTitleText().trim();
 
         Assert.assertEquals("Title is not correct!", expected, actual);
         System.out.println("I see the Dashboard page!");
-        driver.quit();
+        Driver.closeDriver();
     }
 
+    @Then("user should see {string} page")
+    public void user_should_see_page(String string) {
+        String actual = loginPage.getPageSubTitleText().trim();
+        Assert.assertEquals("Page title is not correct!", string, actual);
+    }
 
+    //When user logs in as a "driver" --> public void user_logs_in_as_a(String string) -> loginPage.login(string); -> public void login(String role) { if role == "" do this..}
+    //When user logs in as a "sales manager"
+    //When user logs in as a "store manager"
+    @When("user logs in as a {string}")
+    public void user_logs_in_as_a(String string) {
+        loginPage.login(string);
+    }
 
-}
-/*
+    //  String string = "storemanager85";
+//  String string2 =  "wrong";
+    //When user logs in with "storemanager85" username and "wrong" password
+    @When("user logs in with {string} username and {string} password")
+    public void user_logs_in_with_username_and_password(String string, String string2) {
+        loginPage.login(string, string2);
+    }
+
+    //    String expected = "Invalid user name or password."
+//    Then user verifies that "Invalid user name or password." message is displayed
+    @Then("user verifies that {string} message is displayed")
+    public void user_verifies_that_message_is_displayed(String expected) {
+        String actualResult = loginPage.getWarningMessageText();
+        Assert.assertEquals(expected, actualResult);
+    }
+
+}/*
 Today is October 18, 2020
  Agenda: Introduction to Cucumber BDD
 ########################################
