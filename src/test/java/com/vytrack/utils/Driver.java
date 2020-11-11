ff37858826ebf5058d0f48b4bdd018906f015d0a
numuminov <numuminov@gmail.com>
2020-11-10 20:48:17 -0500

    fourth1

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -12,11 +12,22 @@ import java.net.URL;
 public class Driver {
     private static WebDriver driver;
 
-    private Driver() {}
+    private Driver() {
+    }
 
     public static WebDriver getDriver() {
         if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
+//            jenkins command: test -Dcucumber.filter.tags="@smoke" -Dbrowser="chrome"
+//            custom environment variables: -Dbrowser
+//            -Dproperty  = then read in java System.getProperty("property")
+//            if env variable was specified
+            if (System.getProperty("browser") != null) {
+//                then change browser type
+//                regardless on value configuration.properties
+                System.out.println("Browser type was changed to: " + System.getProperty("browser"));
+                browser = System.getProperty("browser");
+            }
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -31,9 +42,9 @@ public class Driver {
 //                    ChromeOptions chromeOptions = new ChromeOptions();
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
-                        URL gridUrl = new URL("http://54.175.135.91:4444/wd/hub");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
                         driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                     break;
